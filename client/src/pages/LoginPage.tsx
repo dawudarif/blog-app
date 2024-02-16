fa6a20f77e2e16ad7e69dfcdc91bccdb9c3c5f29
dawudarif <dawuddarif@gmail.com>
2024-02-16 22:01:46 +0500

    feat: new pages
    login and register UI

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,3 +1,47 @@
+import { Link } from 'react-router-dom';
+import { Button } from '../components/ui/button';
+import { Input } from '../components/ui/input';
+import '../index.css';
+import axios from 'axios';
+import { useState } from 'react';
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function loginUser() {
+    const response = await axios.post(
+      '/users/login',
+      { email, password },
+      { withCredentials: true },
+    );
+
+    console.log(response);
+  }
+
+  return (
+    <div className='flex flex-col items-center justify-center bg-[#f1f1f1] w-full h-screen gap-10'>
+      <h1 className='font-bold text-3xl text-[#5555]'>Login to Your account</h1>
+      <form className='w-[30%] gap-2 flex flex-col' onSubmit={loginUser}>
+        <Input
+          placeholder='john@example.com'
+          type='email'
+          className='text-base'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder='Password'
+          type='password'
+          className='text-base'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button variant='default' className='w-full mt-4' type='submit'>
+          Login
+        </Button>
+        <Link to='/register'>Not a user? Register here.</Link>
+      </form>
+    </div>
+  );
 }
--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,5 +1,57 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '../components/ui/button';
+import { Input } from '../components/ui/input';
+import '../index.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function RegisterPage() {
-  return <div>RegisterPage</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  async function registerUser() {
+    const response = await axios.post(
+      '/users/register',
+      { email, password },
+      { withCredentials: true },
+    );
+
+    console.log(response);
+  }
+
+  return (
+    <div className='flex flex-col items-center justify-center bg-[#f1f1f1] w-full h-screen gap-10'>
+      <h1 className='font-bold text-3xl text-[#5555]'>
+        Register a New Account
+      </h1>
+      <form className='w-[30%] gap-2 flex flex-col' onSubmit={registerUser}>
+        <Input
+          placeholder='john@example.com'
+          type='email'
+          className='text-base'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder='Password'
+          type='password'
+          className='text-base'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Input
+          placeholder='Confirm Password'
+          type='password'
+          className='text-base'
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <Button variant='default' className='w-full mt-4' type='submit'>
+          Register
+        </Button>
+        <Link to='/login'>Already a user? Login here.</Link>
+      </form>
+    </div>
+  );
 }
