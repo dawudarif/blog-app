f9bd8e9806e73bf1139969c21140f78e0fec2cb2
dawudarif <dawuddarif@gmail.com>
2024-03-13 13:19:52 +0500

    style: footer
    footer is now responsive

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div className='flex justify-center items-center h-16 bg-slate-950 text-white text-lg gap-4 font-mono font-semibold'>
+    <div className='flex justify-center items-center h-16 xs:h-12 sm:h-12 xs:gap-2 sm:gap-2 bg-slate-950 text-white text-lg gap-4 font-mono font-semibold'>
       <span> Blogger</span> <span>&copy; {new Date().getFullYear()}</span>
     </div>
   );
