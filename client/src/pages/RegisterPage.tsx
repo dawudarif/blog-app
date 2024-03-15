771ca11622dbdf14d1f5965d2469a8ca1df423b3
dawudarif <dawuddarif@gmail.com>
2024-03-15 22:30:14 +0500

    style: register page
    register page is now responsive and improved styling

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -59,49 +59,52 @@ export default function RegisterPage() {
   }
 
   return (
-    <div className='flex flex-col items-center justify-center bg-slate-50 w-full h-[35rem] gap-10'>
+    <div className='flex flex-col items-center justify-center bg-slate-50 w-full h-[38rem] gap-10'>
       <div className='flex flex-col justify-center items-center gap-6'>
-        <h1 className='font-bold text-3xl text-[#5555]'>
+        <h1 className='font-bold text-3xl sm:text-2xl xs:text-2xl text-[#5555]'>
           Register a New Account
         </h1>
         <KeyRound size={45} color='#5555' />
       </div>
-      <form className='w-[30%] gap-2 flex flex-col' onSubmit={registerUser}>
+      <form
+        className='w-[40%] sm:w-[95%] xs:w-[95%] md:w-[50%] gap-2 flex flex-col h-auto'
+        onSubmit={registerUser}
+      >
         <Input
           placeholder='john@example.com'
           type='email'
-          className='text-base'
+          className='text-base p-6 focus-visible:ring-0 focus-visible:ring-offset-0'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
         <Input
           placeholder='John Doe'
           type='text'
-          className='text-base'
+          className='text-base p-6 focus-visible:ring-0 focus-visible:ring-offset-0'
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
         <Input
           placeholder='Password'
           type='password'
-          className='text-base'
+          className='text-base p-6 focus-visible:ring-0 focus-visible:ring-offset-0'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
         <Input
           placeholder='Confirm Password'
           type='password'
-          className='text-base'
+          className='text-base p-6 focus-visible:ring-0 focus-visible:ring-offset-0'
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
         />
         <Button
           variant='default'
-          className='w-full mt-4'
+          className='w-full mt-4 p-6'
           type='submit'
           disabled={loading}
         >
-          {loading ? <RingLoader size={25} color='white' /> : <>Login</>}
+          {loading ? <RingLoader size={25} color='white' /> : <>Register</>}
         </Button>
         <Link to='/login' className='hover:text-blue-900'>
           Already a user? Login here.
