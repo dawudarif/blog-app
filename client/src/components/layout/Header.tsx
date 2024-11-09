075cc2fe1cf16add1867850baa8759a414587ad8
Dawood Arif <dawuddarif@gmail.com>
2024-11-09 11:14:09 +0500

    style: header redesign

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,18 +1,19 @@
-import axios from 'axios';
-import { useContext, useEffect, useState } from 'react';
-import { Link, Navigate } from 'react-router-dom';
-import { UserContext } from '../../context/userContext';
-import { toast } from 'sonner';
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import { UserContext } from "../../context/userContext";
+import { toast } from "sonner";
+import clsx from "clsx";
 
 export default function Header() {
   const { userInfo, setUserInfo } = useContext(UserContext) as any;
   const [redirect, setRedirect] = useState(false);
 
   const hrefStyle =
-    'font-semibold text-xl sm:text-lg xs:text-lg cursor-pointer hover:bg-slate-800 transition-all duration-500 p-2 xs:p-1 sm:p-1 rounded-md text-white';
+    "text-xl font-semibold cursor-pointer hover:bg-stone-100 transition-all duration-500 rounded-md text-brand-text-black p-2";
 
   async function getProfile() {
-    const response = await axios.get('/users/profile', {
+    const response = await axios.get("/users/profile", {
       withCredentials: true,
     });
 
@@ -24,7 +25,7 @@ export default function Header() {
   }
 
   async function logout() {
-    const response = await axios.get('/users/logout', {
+    const response = await axios.get("/users/logout", {
       withCredentials: true,
     });
 
@@ -40,34 +41,37 @@ export default function Header() {
   }, []);
 
   if (redirect) {
-    return <Navigate to='/login' />;
+    return <Navigate to="/login" />;
   }
 
   return (
-    <div className='flex justify-between sm:justify-center xs:justify-center px-2 items-center h-20 xs:h-15 sm:h-15  bg-slate-950 text-white shadow-sm'>
+    <div className="flex max-w-screen-xxxl text-brand-text-black sm:justify-between justify-center items-center sm:flex-row flex-col py-4 px-5 lg:px-10">
       <span>
-        <Link to='/' className={hrefStyle}>
-          Feed
-        </Link>
-        <Link to='/search' className={hrefStyle}>
-          Search
-        </Link>
-        {userInfo.name && (
-          <Link to='/blog/create' className={hrefStyle}>
-            Create
-          </Link>
+        {userInfo?.name && (
+          <p className={clsx("capitalize !font-bold text-3xl", hrefStyle)}>
+            {userInfo.name}
+          </p>
         )}
       </span>
 
       <span>
+        <Link to="/" className={hrefStyle}>
+          Feed
+        </Link>
+        <Link to="/search" className={hrefStyle}>
+          Search
+        </Link>
+        {userInfo.name && (
+          <Link to="/blog/create" className={hrefStyle}>
+            Create
+          </Link>
+        )}
         {userInfo.name ? (
-          <>
-            <div className={hrefStyle} onClick={logout}>
-              Logout
-            </div>
-          </>
+          <span className={hrefStyle} onClick={logout}>
+            Logout
+          </span>
         ) : (
-          <Link to='/login' className={hrefStyle}>
+          <Link to="/login" className={hrefStyle}>
             Login
           </Link>
         )}
